94ca23fa08f70ffa8b7f79651ffab0045a6be45b
yuvvinn <iyubin284@gmail.com>
2026-02-19 14:53:38 +0900

    chore: Vite + React + TypeScript 초기 설정 (Emotion, Zustand, TanStack Query, Axios, Vercel)
    chore: Vite + React + TypeScript 초기 설정
    
    - pnpm, Vite 기반 프로젝트 구성
    - Emotion CSS, Zustand, TanStack Query, Axios 의존성 추가

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+function App() {
+  return (
+    <div
+      css={css`
+        padding: 2rem;
+        text-align: center;
+      `}
+    >
+      <h1>Prography 11th Frontend</h1>
+      <p>Vite + React + TypeScript + Emotion + Zustand + TanStack Query + Axios</p>
+    </div>
+  )
+}
+
+export default App
